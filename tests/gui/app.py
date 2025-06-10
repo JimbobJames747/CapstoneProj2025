31105fd39c573c8b6e48b8c7b678eb06753a9992
James McDougall <jamesmcdougall747@gmail.com>
2025-06-10 23:05:56 +0100

    Clean up and adding .ui form

--- tests/gui/app.py
+++ tests/gui/app.py
@@ -1,39 +1,9 @@
-from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QAction, QIcon, QKeySequence
-from PySide6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QLabel,
-    QMainWindow,
-    QStatusBar,
-    QToolBar,
-)
+import sys
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtUiTools import QUiLoader
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Simulator")
-
-        label = QLabel("yo")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("Toolbar")
-        self.addToolBar(toolbar)
-
-        button_action = QAction("Button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.toolbar_button_clicked)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
-
-        self.setStatusBar(QStatusBar(self))
-
-    def toolbar_button_clicked(self, s):
-        print("click", s)
-
-app = QApplication([])
-window = MainWindow()
+loader = QUiLoader()
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load("mainwindow.ui", None)
 window.show()
 app.exec()
