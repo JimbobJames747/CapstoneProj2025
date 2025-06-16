2d3a7ae7b1a638a98eb1c3091b407a834b348e71
James McDougall <jamesmcdougall747@gmail.com>
2025-06-16 16:48:33 +0100

    setting up graphicsview from drag and drop

--- src/gui/app.py
+++ src/gui/app.py
@@ -1,8 +1,13 @@
 import os
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtWidgets import QGraphicsScene, QLabel, QGraphicsView
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QFile
+from PySide6.QtGui import QDrag
+from PySide6.QtCore import QFile, QMimeData
+
+
+
 from .slots import SlotHandler
 
 
@@ -20,14 +25,21 @@ class MainApp:
         self.window = loader.load(ui_file_path, None)
 
 
+        # set up basic logic handling
         self.logic = SlotHandler(self.window)
-        
         #self.window.pushButton.clicked.connect(lambda: self.logic.change_label("test"))
-        
         # menu bar file-->exit
         self.window.actionExit.triggered.connect(self.logic.exit_app)
 
 
+        # set up drag and drop area (graphics scene)
+        self.scene = QGraphicsScene()
+        self.view = self.window.findChild(QGraphicsView, "graphicsView")
+        self.view.setScene(self.scene)
+
+
+
+    # start gui
     def run(self):
         self.window.showMaximized()
         self.app.exec()
