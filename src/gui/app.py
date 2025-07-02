8e3610b5c99219f0ba99cd82ff843d28c0b7069e
James McDougall <jamesmcdougall747@gmail.com>
2025-07-02 17:05:45 +0100

    added qrc file

--- src/gui/app.py
+++ src/gui/app.py
@@ -4,7 +4,10 @@ from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtWidgets import QGraphicsScene, QLabel, QGraphicsView
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtGui import QDrag
-from PySide6.QtCore import QFile, QMimeData
+from PySide6.QtCore import QFile, QMimeData, QResource
+from PySide6.QtGui import QPixmap
+import gui.res.resources
+
 
 
 
@@ -25,12 +28,31 @@ class MainApp:
         self.window = loader.load(ui_file_path, None)
 
 
+
         # set up basic logic handling
         self.logic = SlotHandler(self.window)
         #self.window.pushButton.clicked.connect(lambda: self.logic.change_label("test"))
         # menu bar file-->exit
         self.window.actionExit.triggered.connect(self.logic.exit_app)
 
+
+        label_3 = self.window.findChild(QLabel, "label_3")
+        pixmap = QPixmap(":/img/test.png")  # Use the resource path here
+        label_3.setPixmap(pixmap) 
+
+        
+        res = QResource(":/src/gui/res/img/test.png")
+        print("Resource exists:", res.isValid())
+
+        print(QResource(":/src/gui/res/img/test.png").isValid())
+        print(QResource(":/src/gui/res/img/test.png").isValid())
+        print(QResource(":/src/gui/res/img/test.png").isValid())
+        print(QResource(":/src/gui/res/img/test.png").isValid())
+
+        print("Pixmap is null?", pixmap.isNull())
+
+        f = QFile("src/gui/res/img/test.png")
+        print("Resource file exists?", f.exists())
 
         # set up drag and drop area (graphics scene)
         self.scene = QGraphicsScene()
